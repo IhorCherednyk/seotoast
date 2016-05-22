c4dc3d31732614f938c9e09b749c593cbe75eb0e
Ihor <ihor_cherednyk@mail.ru>
2016-05-22 21:02:27 +0300

    check

---
+++ js/common.js
@@ -0,0 +1,50 @@
+$(function() {
+
+	function heightDetect() {
+		$(".main-header").css("height", $(window).height());
+	};
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});
+
+	$(".toggle_mnu").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
+
+	$(".toggle_mnu").click(function() {
+		if ($(".main-nav").is(":visible")) {
+			$(".main-nav").fadeOut(600);
+			$(".main-nav li").removeClass("fadeInDown animated")
+		} else {
+			$(".main-nav").fadeIn(600);
+			$(".main-nav li").addClass("fadeInDown animated")
+		};
+	});
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
